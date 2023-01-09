e0ab67634913f57590eba393c976878e6dc7df45
Abhishek Mishra <abhishekmishra9051@gmail.com>
2023-01-09 23:12:08 +0530

    Solution || 08-01-2023 || 144. Binary Tree Preorder Traversal

---
+++ LeetCode - Jan/Problem 144.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorderTraversal(root, result);
+        return result;
+    }
+
+    private void preorderTraversal(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+
+        result.add(node.val);
+        preorderTraversal(node.left, result);
+        preorderTraversal(node.right, result);
+    }
+}
