3d3d8835adc290c7034fbc618b1a737e3b351fc3
Abhishek Mishra <abhishekmishra9051@gmail.com>
2026-03-21 08:13:35 +0530

    Removed the unused code

--- Arrays/Longest Substring.java
+++ Arrays/Longest Substring.java
@@ -1,4 +1,5 @@
-
+package Arrays;
+/*
 3. Longest Substring Without Repeating Characters
 Given a string s, find the length of the longest substring without repeating characters.
 
@@ -7,7 +8,10 @@ Example 1:
 Input: s = "abcabcbb"
 Output: 3
 Explanation: The answer is "abc", with the length of 3.
+*/
 
+
+import java.util.HashMap;
 
 class Solution {
     // Method to find the length of the longest substring without repeating characters
